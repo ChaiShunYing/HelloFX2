bd54d2ebfb7d7940d730ba85771365cc20848e8b
ChaiShunYing <147686250+ChaiShunYing@users.noreply.github.com>
2024-12-09 16:36:38 +0800

    Update OnlinePrintingInterface.java

--- src/Project_CSC3104/OnlinePrintingInterface.java
+++ src/Project_CSC3104/OnlinePrintingInterface.java
@@ -68,6 +68,7 @@ public class OnlinePrintingInterface extends Application {
         primaryStage.setTitle("Customer Details");
         primaryStage.setScene(scene);
         primaryStage.show();
+        hahaah
     }
 
     // Stage 2: Print Options
